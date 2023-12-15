e55d57ae913cae1158887cf3af41ecf995c19de1
Miriam Trapero Rodríguez <90465230+Mimitrz@users.noreply.github.com>
2023-12-15 19:20:06 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Imprimir en la consola algo
+        print("Hola mundo");
+       // print(22);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
